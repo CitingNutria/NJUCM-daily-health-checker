e7df1225b801e180e68d59c1b46fda09d7f75659
CitingNutria <37170862+CitingNutria@users.noreply.github.com>
2021-09-05 07:26:24 +0800

    Update AutoCheck.py

--- AutoCheck.py
+++ AutoCheck.py
@@ -6,24 +6,7 @@ import time
 import os
 
 def healthCheck(name,password,userid,location):
-    headers = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-        'Cache-Control': 'max-age=0',
-        'Connection': 'keep-alive',
-        'Cookie': '',
-        'DNT': '1',
-        'Host': 'pdc.njucm.edu.cn',
-        'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
-        'sec-ch-ua-mobile': '?0',
-        'Sec-Fetch-Dest': 'document',
-        'Sec-Fetch-Mode': 'navigate',
-        'Sec-Fetch-Site': 'none',
-        'Sec-Fetch-User': '?1',
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
-    }
+
 
     postData = {
         "username": userid,
